02bea2f4fdf24eeae7e19c0c780bddaa2481c68e
Camilo Manzur <camilomanzur@192.168.0.112>
2026-03-15 12:04:40 -0600

    Keep newsletter flow fully inside the website

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -20,6 +20,152 @@
 			.replace(/'/g, "&#39;");
 	}
 
+	function sanitizeBeehiivContent(html) {
+		if (!html || typeof html !== "string") return "";
+		var parser = new DOMParser();
+		var doc = parser.parseFromString("<div>" + html + "</div>", "text/html");
+		var root = doc.body.firstElementChild;
+		if (!root) return "";
+
+		function cleanNode(node, ownerDoc) {
+			if (node.nodeType === Node.TEXT_NODE) {
+				return ownerDoc.createTextNode(node.textContent || "");
+			}
+
+			if (node.nodeType !== Node.ELEMENT_NODE) {
+				return null;
+			}
+
+			var tag = node.tagName.toLowerCase();
+			var passthrough = {
+				p: true,
+				h1: true,
+				h2: true,
+				h3: true,
+				h4: true,
+				ul: true,
+				ol: true,
+				li: true,
+				blockquote: true,
+				strong: true,
+				b: true,
+				em: true,
+				i: true,
+				br: true,
+				hr: true,
+				picture: true,
+				source: true,
+				img: true,
+				a: true,
+				iframe: true
+			};
+
+			if (!passthrough[tag]) {
+				var fragment = ownerDoc.createDocumentFragment();
+				for (var i = 0; i < node.childNodes.length; i += 1) {
+					var cleanedChild = cleanNode(node.childNodes[i], ownerDoc);
+					if (cleanedChild) fragment.appendChild(cleanedChild);
+				}
+				return fragment;
+			}
+
+			var element = ownerDoc.createElement(tag);
+
+			if (tag === "a") {
+				var href = node.getAttribute("href");
+				if (href) {
+					if (/enfrio\.madridtotal\.mx/i.test(href)) {
+						element.setAttribute("href", "#article-subscribe");
+						element.className = "article-subscribe-link";
+					} else {
+						element.setAttribute("href", href);
+					}
+					if (/^https?:\/\//i.test(href) && !/enfrio\.madridtotal\.mx/i.test(href)) {
+						element.setAttribute("target", "_blank");
+						element.setAttribute("rel", "noopener noreferrer");
+					}
+				}
+			}
+
+			if (tag === "blockquote" && node.classList.contains("twitter-tweet")) {
+				element.className = "twitter-tweet";
+			}
+
+			if (tag === "img") {
+				var src = node.getAttribute("src");
+				if (src) element.setAttribute("src", src);
+				var alt = node.getAttribute("alt");
+				element.setAttribute("alt", alt || "");
+				element.setAttribute("loading", "lazy");
+			}
+
+			if (tag === "source") {
+				var srcset = node.getAttribute("srcset");
+				if (srcset) element.setAttribute("srcset", srcset);
+				var media = node.getAttribute("media");
+				if (media) element.setAttribute("media", media);
+			}
+
+			if (tag === "iframe") {
+				var iframeSrc = node.getAttribute("src");
+				if (!iframeSrc) return null;
+				element.setAttribute("src", iframeSrc);
+				element.setAttribute("loading", "lazy");
+				element.setAttribute("referrerpolicy", "no-referrer-when-downgrade");
+				element.setAttribute("allowfullscreen", "");
+			}
+
+			for (var j = 0; j < node.childNodes.length; j += 1) {
+				var child = cleanNode(node.childNodes[j], ownerDoc);
+				if (child) element.appendChild(child);
+			}
+
+			if ((tag === "p" || tag === "li") && !element.textContent.trim() && !element.querySelector("img, iframe")) {
+				return null;
+			}
+
+			return element;
+		}
+
+		var cleanDoc = document.implementation.createHTMLDocument("");
+		var container = cleanDoc.createElement("div");
+
+		for (var i = 0; i < root.childNodes.length; i += 1) {
+			var cleanChild = cleanNode(root.childNodes[i], cleanDoc);
+			if (cleanChild) container.appendChild(cleanChild);
+		}
+
+		return container.innerHTML;
+	}
+
+	function ensureXWidgets(container) {
+		if (!container || !container.querySelector(".twitter-tweet")) return;
+
+		function loadWidgets() {
+			if (window.twttr && window.twttr.widgets && typeof window.twttr.widgets.load === "function") {
+				window.twttr.widgets.load(container);
+			}
+		}
+
+		if (window.twttr && window.twttr.widgets) {
+			loadWidgets();
+			return;
+		}
+
+		if (!document.getElementById("twitter-wjs")) {
+			var script = document.createElement("script");
+			script.id = "twitter-wjs";
+			script.async = true;
+			script.defer = true;
+			script.src = "https://platform.twitter.com/widgets.js";
+			script.onload = loadWidgets;
+			document.body.appendChild(script);
+			return;
+		}
+
+		window.setTimeout(loadWidgets, 250);
+	}
+
 	function renderFeed(data) {
 		var status = document.getElementById("newsletter-status");
 		var featured = document.getElementById("newsletter-featured");
@@ -50,7 +196,7 @@
 			}).join("") + '</div>',
 			'<div class="newsletter-card-actions">',
 			'<a class="button primary" href="newsletter-post.html?slug=' + encodeURIComponent(primary.slug) + '">Leer dentro de Madrid Total</a>',
-			'<a class="newsletter-inline-link" href="' + escapeHtml(primary.web_url) + '" target="_blank" rel="noopener noreferrer">Abrir original</a>',
+			'<a class="newsletter-inline-link" href="#signup-title">Suscribirme a EN FRIO</a>',
 			'</div>',
 			'</div>'
 		].join("");
@@ -92,9 +238,6 @@
 		status.hidden = true;
 		article.hidden = false;
 
-		var externalLink = document.getElementById("article-external-link");
-		if (externalLink) externalLink.href = post.web_url;
-
 		article.className = "newsletter-article";
 		article.innerHTML = [
 			'<header class="article-header">',
@@ -104,12 +247,14 @@
 			'<p class="article-excerpt">' + escapeHtml(post.excerpt) + '</p>',
 			'<div class="article-cover"><img src="' + escapeHtml(post.cover_image_url) + '" alt="' + escapeHtml(post.title) + '"></div>',
 			'</header>',
-			'<div class="article-body">' + (post.content_html || "") + '</div>',
+			'<div class="article-body">' + sanitizeBeehiivContent(post.content_html || "") + '</div>',
 			'<div class="newsletter-card-actions">',
 			'<a class="button ghost-light" href="newsletter.html">Volver al archivo</a>',
-			'<a class="button primary" href="' + escapeHtml(post.web_url) + '" target="_blank" rel="noopener noreferrer">Abrir original</a>',
+			'<a class="button primary" href="#article-subscribe">Suscribirme a EN FRIO</a>',
 			'</div>'
 		].join("");
+
+		ensureXWidgets(article);
 
 		document.title = "Madrid Total | " + post.title;
 	}
