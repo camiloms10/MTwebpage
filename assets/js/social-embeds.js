3d2287002570fe330a233b17672e5dbd6fca5898
camiloms10 <cmanzur1060@gmail.com>
2026-03-17 12:24:19 -0600

    Replace social proof cards with real social embeds

---
+++ assets/js/social-embeds.js
@@ -0,0 +1,49 @@
+(function () {
+	function loadScript(src, onLoad) {
+		var script = document.querySelector('script[src="' + src + '"]');
+		if (script) {
+			if (script.dataset.loaded === "true") {
+				onLoad();
+				return;
+			}
+
+			script.addEventListener("load", onLoad, { once: true });
+			return;
+		}
+
+		script = document.createElement("script");
+		script.src = src;
+		script.async = true;
+		script.addEventListener("load", function () {
+			script.dataset.loaded = "true";
+			onLoad();
+		}, { once: true });
+		document.body.appendChild(script);
+	}
+
+	function initEmbeds() {
+		var twitterBlock = document.querySelector(".twitter-tweet");
+		if (twitterBlock) {
+			loadScript("https://platform.twitter.com/widgets.js", function () {
+				if (window.twttr && window.twttr.widgets) {
+					window.twttr.widgets.load();
+				}
+			});
+		}
+
+		var instagramBlock = document.querySelector(".instagram-media");
+		if (instagramBlock) {
+			loadScript("https://www.instagram.com/embed.js", function () {
+				if (window.instgrm && window.instgrm.Embeds) {
+					window.instgrm.Embeds.process();
+				}
+			});
+		}
+	}
+
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", initEmbeds, { once: true });
+	} else {
+		initEmbeds();
+	}
+})();
